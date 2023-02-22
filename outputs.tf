b5c29899a6dde690059cf534369c092ead541194
FaisalAn <anm.faisal@health.qld.gov.au>
2023-02-22 05:58:46 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
-
-utput "ips" {
+Output "ips" {
   #value = libvirt_domain.domain-ubuntu
   #value = libvirt_domain.domain-ubuntu.*.network_interface
   # show IP, run 'terraform refresh' if not populated
